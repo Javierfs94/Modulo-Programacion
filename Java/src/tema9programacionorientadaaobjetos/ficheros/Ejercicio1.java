653fd31d372643e1a8463815d773afdbf658af00
Javierfs94 <javifs94@gmail.com>
2019-03-27 12:31:09 +0100

    Subida refactorización ejercicio 1 de ficheros

--- Java/src/tema9programacionorientadaaobjetos/ficheros/Ejercicio1.java
+++ Java/src/tema9programacionorientadaaobjetos/ficheros/Ejercicio1.java
@@ -17,31 +17,56 @@ public class Ejercicio1 {
 
   public static void main(String[] args) {
 
-    boolean esPrimo;
     int rangoInicial = 1;
     int rangoFinal = 500;
-    BufferedWriter bw;
+    String localizacion = "C:\\Users\\d18frsej2\\eclipse-workspace\\PGNJava\\src\\tema9programacionorientadaaobjetos\\ficheros\\primos.txt";
     try {
-      bw = new BufferedWriter(new FileWriter("primos.dat"));
+      BufferedWriter bw = new BufferedWriter(new FileWriter(localizacion));
 
-      for (int i = rangoInicial; i <= rangoFinal; i++) {
-        esPrimo = true;
-        for (int j = 2; j < i; j++) {
-          if (i % j == 0) {
-            esPrimo = false;
-          }
-        }
-        if (esPrimo) {
-          System.out.println(i + "\n");
-          bw.write(i + "\n");
-        }
-      }
+      calcularPrimos(rangoInicial, rangoFinal, bw);
       System.out.println("Archivo guardado con éxito");
       bw.close();
     } catch (IOException e) {
       System.err.println("No se ha podido escribir el archivo");
     }
 
+  }
+
+  /**
+   * Calcular los números primos
+   * 
+   * @param rangoInicial
+   * @param rangoFinal
+   * @param bw
+   * @throws IOException
+   */
+  public static void calcularPrimos(int rangoInicial, int rangoFinal, BufferedWriter bw) throws IOException {
+    boolean esPrimo;
+    for (int i = rangoInicial; i <= rangoFinal; i++) {
+      esPrimo = true;
+      for (int j = 2; j < i; j++) {
+        if (i % j == 0) {
+          esPrimo = false;
+        }
+      }
+      comprobarPrimo(esPrimo, bw, i);
+    }
+  }
+
+  /**
+   * Comprueba si un número es primo o no
+   * 
+   * @param esPrimo
+   * @param bw
+   * @param i
+   * @throws IOException
+   */
+  public static void comprobarPrimo(boolean esPrimo, BufferedWriter bw, int i) throws IOException {
+
+    if (esPrimo) {
+//      System.out.println(i + "\n");
+      bw.write(i + "\n");
+    }
   }
 
 }
